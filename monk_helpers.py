59ea102a98d1b5b026712f5fedb2021baaaa33ad
marco <mac.uchiha96@gmail.com>
2024-01-14 02:48:01 +0100

    NN Keras Monk1-2-3

--- monk_helpers.py
+++ monk_helpers.py
@@ -1,9 +1,12 @@
 import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+
 def read_ds(path):
   """
   parse CSV data set and
   returns a tuple (input, target)
   """
+
   df = pd.read_csv(path, sep=" ", names=['NaN','y','x1','x2','x3','x4','x5','x6','garbage'])
   y, df = df['y'], df.drop(columns=['NaN','garbage','y'])
   
@@ -11,3 +14,8 @@ def read_ds(path):
   df = pd.get_dummies(df, columns=['x1','x2','x3','x4','x5','x6']).astype('int')
   return (df, y)
 
+### Global K-Fold strategy
+CV = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+
+### Initialization seed
+SEED = 42
